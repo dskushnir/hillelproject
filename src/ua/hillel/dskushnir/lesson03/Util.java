4f6751340bf76c4f3775aca1c6547374f0b722af
dskushnir <kpds7175@gmail.com>
2018-12-05 17:49:57 +0200

    init util

--- src/ua/hillel/dskushnir/lesson03/Util.java
+++ src/ua/hillel/dskushnir/lesson03/Util.java
@@ -1,27 +1,23 @@
 package ua.hillel.dskushnir.lesson03;
 
-public class Task3 {
-
-    public static void main(String[] args) {
-        String string = "Not New York, Roy went on";
-        if (isPalindrom(string)) {
-            System.out.println(string + " -  It is a palindrome ");
-        } else System.out.println(string + " - It is not  palindrome ");
-
-    }
-
-    private static boolean isPalindrom(String string) {
+public class Util {
+        public static boolean isPalindrom(String string) {
         String convert = string.replaceAll("[^A-Za-z]+", "").toLowerCase();
         if ( convert.isEmpty()) {
+            System.out.println("String is empty");
             return false;
+
         }
         int leftPosition = 0;
         int rightPosition = convert.length() - 1;
         while (leftPosition < rightPosition) {
             if (convert.charAt(leftPosition++) != convert.charAt(rightPosition--)) {
+                System.out.println();
+                System.out.println(string + " - It is not  palindrome ");
                 return false;
             }
         }
+            System.out.println(string + " -  It is a palindrome ");
         return true;
     }
 }
