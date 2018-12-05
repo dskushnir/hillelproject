f5a982f553f6a7dd1cf0b39849968d7a59d95f6d
dskushnir <kpds7175@gmail.com>
2018-12-05 18:09:51 +0200

    call add util as a jar

--- src/ua/hillel/dskushnir/lesson03/Task3.java
+++ src/ua/hillel/dskushnir/lesson03/Task3.java
@@ -3,25 +3,12 @@ package ua.hillel.dskushnir.lesson03;
 public class Task3 {
 
     public static void main(String[] args) {
-        String string = "Not New York, Roy went on";
-        if (isPalindrom(string)) {
-            System.out.println(string + " -  It is a palindrome ");
-        } else System.out.println(string + " - It is not  palindrome ");
+        callIsPalindrom("Race fast, safe car");
 
     }
-
-    private static boolean isPalindrom(String string) {
-        String convert = string.replaceAll("[^A-Za-z]+", "").toLowerCase();
-        if ( convert.isEmpty()) {
-            return false;
-        }
-        int leftPosition = 0;
-        int rightPosition = convert.length() - 1;
-        while (leftPosition < rightPosition) {
-            if (convert.charAt(leftPosition++) != convert.charAt(rightPosition--)) {
-                return false;
-            }
-        }
-        return true;
+    private static void callIsPalindrom(String string){
+        Util.isPalindrom(string);
     }
+
+
 }
