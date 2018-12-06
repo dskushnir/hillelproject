b0903cd4ef176e6e9cc24225d4c1dfec811cb665
dskushnir <kpds7175@gmail.com>
2018-12-06 10:09:51 +0200

    corrections lesson 03 Task1

--- src/ua/hillel/dskushnir/lesson03/Task1.java
+++ src/ua/hillel/dskushnir/lesson03/Task1.java
@@ -4,31 +4,24 @@ public class Task1 {
     public static void main(String[] args) {
         char ch1 = '*';
         char ch2 = ' ';
-        String string =
+        String initials =
                 "* * * *  \n" + "*    * \n" + "*   *  *\n" + "* *    *\n" + "*   *  *\n" + "*    * \n" + "* * * *";
-        System.out.println(string);
-        countSymbol(string, ch1, ch2);
-
+        System.out.println(initials);
+        countSymbol(initials, ch1);
+        countSymbol(initials,ch2);
     }
-
-    private static void countSymbol(String string, char ch1, char ch2) {
-        int counter1 = 0;
-        int counter2 = 0;
+    private static void countSymbol(String string, char ch ){
+        int counter = 0;
         if (string.isEmpty()) {
             System.out.println("String is empty");
         } else {
             for (int i = 0; i < string.length(); i++) {
-                if (string.charAt(i) == ch1) {
-                    counter1++;
+                if (string.charAt(i) == ch) {
+                    counter++;
                 }
-                if (string.charAt(i) == ch2) {
-                    counter2++;
-                }
-            }
-            System.out.println("Characters * =" + counter1 + ", characters ' ' =" + counter2);
 
+            }
+            System.out.println("Characters" + ch + "=" + counter);
         }
     }
-
-
 }
